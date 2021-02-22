24cced0a973a5a3f127c3ab315b28abac703f1fb
Min Gee Suh <suh@student.unimelb.edu.au>
2021-02-22 19:40:12 +0900

    refactor: updated main.ts

--- Client/src/ts/main.ts
+++ Client/src/ts/main.ts
@@ -1,20 +1,13 @@
 import bookstoreRender from './bookstoreCarousel';
 import imageHandler from "./imageHandler";
-<<<<<<< HEAD
 import mapHandler from "./mapHandler";
 
 import './firebaseSetting'
-=======
->>>>>>> 1e5114b81769e3c83b7ce5ec97472f7bea294f02
 import './auth'
 import './login'
 import fireStore from './db';
 
 // bookstoreRender();
 imageHandler();
-<<<<<<< HEAD
 mapHandler();
-
-=======
 fireStore();
->>>>>>> 1e5114b81769e3c83b7ce5ec97472f7bea294f02
